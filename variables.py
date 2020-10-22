05fddc3915211833072c44e0672ba82c38fabd2f
AhmadFouda <72926642+AhmadFouda@users.noreply.github.com>
2020-10-22 20:53:20 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,51 @@
+# Variables
+
+Define a Variable of typee boool???
+Answer :( true . false )
+print(type(5 == 5)          # an example: ture
+
+
+
+* whate iss the benfit of typee method?     => iss usefule to know how to use variable
+
+
+
+can we use semi colon ; wwith python ?     => yes , but littel situations
+
+
+
+pyhon iss interpreted orr compiled ?        => interpreter
+
+
+
+What iss the differences between low level & high level languages ?
+Answer : 
+ 
+ low level computer language 
+uses 0,1 the binary system
+easy to be executed
+hard to program 
+examples => 'c' , 'assembly'
+
+ high level computer language 
+takes less time
+easy to be corrected
+easy to program 
+examples => 'c#' , 'python'
+
+Python Operators Tasks
+
+What iss the differences between = , == ?
+ =  a assignment operator  mean equal
+ == a comparison operator  mean question iff it equal
+
+
+What do we mean by using != ?
+ !=  mean nott equal
+
+
+What iss the operator precedence ?
+()
+**
+* , /
++ , -
